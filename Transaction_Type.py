b3e902ea96e4717106e8d1dc1b3afed61a334af1
Ivana <test.bmohb08@gmail.com>
2016-02-02 14:54:11 -0600

    Update Transaction_Type.py

--- Transaction_Type.py
+++ Transaction_Type.py
@@ -21,3 +21,6 @@ Credits_ATM = ['Cash Payment','Wire Payment','ACH Payment','Paper Check','e-Chec
 Merchant_Debit = ['Interest'] + ['Purchase'] * 9
 Credits_Ovpmt_Refund = ['Cash Payment','Wire Payment','Paper Check','e-Check','Online Transfer','ATM Payment','Reversal','Award'] + ['ACH Payment'] * 2
 Credits_Payments = ['Cash Payment','Wire Payment'] * 2 + ['ACH Payment'] * 4
+Credits_Refund_Red = ['Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Cash Payment','Wire Payment','ACH Payment','ACH Payment','Paper Check','e-Check','Online Transfer','ATM Payment','Reversal','Award']
+Credits_Refund_Yellow = ['Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Refund','Cash Payment','Wire Payment','ACH Payment','ACH Payment','Paper Check','e-Check','Online Transfer','ATM Payment','Reversal','Award']
+Credits_Refund_Green = ['Refund','Refund','Refund','Refund','Refund','Refund','Cash Payment','Wire Payment','ACH Payment','ACH Payment','Paper Check','e-Check','Online Transfer','ATM Payment','Reversal','Award']
