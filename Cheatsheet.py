6586a7223d9aa7479a7d3e780fd1956dce52903a
Ivana <test.bmohb08@gmail.com>
2016-04-04 16:33:47 -0500

    Create Cheatsheet.py

---
+++ Cheatsheet.py
@@ -0,0 +1,7 @@
+For BMO Hadoop Cluster:
+Python Files using Barnum and Faker must in the folder containing the Barnum and Faker files, currently:
+/home/mrains/cc_hadoop/venv/lib/python2.7/site-packages
+
+For each new session deactivate python 2.6 and activate 2.7:
+deactivate
+source ./venv/bin/activate
